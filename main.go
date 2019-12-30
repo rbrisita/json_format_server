54a96961a70e425a568884dc4e0adac19741938f
Robert Brisita <rbrisita@hotmail.com>
2019-12-30 16:45:52 -0500

    Adding libraries and code for round trip AJAX communication. Added the copy functionality for server AJAX response.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"text/template"
@@ -8,11 +10,11 @@ import (
 
 var templates = template.Must(template.ParseFiles("view.tmpl"))
 
-type JSONFormat struct {
+type JSONInput struct {
 	Data string
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, json_format *JSONFormat) {
+func renderTemplate(w http.ResponseWriter, tmpl string, json_format *JSONInput) {
 	err := templates.ExecuteTemplate(w, tmpl + ".tmpl", json_format)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -20,10 +22,40 @@ func renderTemplate(w http.ResponseWriter, tmpl string, json_format *JSONFormat)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "view", &JSONFormat{Data: ""})
+	renderTemplate(w, "view", &JSONInput{Data: ""})
+}
+
+func manHandler(w http.ResponseWriter, r *http.Request) {
+	var t JSONInput
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		t.Data = err.Error()
+	}
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    w.Write([]byte(`{"message": "post man called", "output": "` + t.Data + `"}`))
+}
+
+func libHandler(w http.ResponseWriter, r *http.Request) {
+	var t JSONInput
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		t.Data = err.Error()
+	}
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post lib called", "output": "` + t.Data + `"}`))
 }
 
 func main() {
-	http.HandleFunc("/", viewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", viewHandler)
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/man", manHandler).Methods(http.MethodPost)
+	api.HandleFunc("/lib", libHandler).Methods(http.MethodPost)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
